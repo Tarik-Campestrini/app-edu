6d7d3e998a11730f9b9d7353201d424bda907cc5
Tarik Campestrini <tarikrc@hotmail.com>
2024-11-17 16:38:11 -0300

    configuração

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object("config.DevelopmentConfig")
+app.config.from_object("config.ProductionConfig")
 db = SQLAlchemy(app)
 
 app.app_context().push()
--- config.py
+++ config.py
@@ -5,9 +5,7 @@ class Config():
   
   
 #Configuração DataBase
-  usuario = "root"
-  senha = "root123"
-  porta = 3306
+  
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_DATABASE_URI = "mysql+pymysql://u725745694_trk:MgT+i8#0Xt@srv1196.hstgr.io:3306/u725745694_app_edu"
   #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/app-edu"
